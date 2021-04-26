02aa52e8ed51ebf9d7c27cbe857bd773cba0d1bb
ning <1722436951@qq.com>
2021-04-26 23:10:19 +0800

    a

---
+++ src/main/java/com/example/demo/test/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.example.demo.test;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloWorld {
+    @RequestMapping("helloWorld")
+    public String helloWorld(){
+        return "HelloWorld";
+    }
+}
